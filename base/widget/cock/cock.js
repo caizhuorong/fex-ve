026ec68da8bd37884be7b013bc51b79b943c848e
zsl99a <1033392732@qq.com>
2015-11-19 11:26:26 +0800

    代码更新

--- base/widget/cock/cock.js
+++ base/widget/cock/cock.js
@@ -10,9 +10,64 @@ var tpl = require('common:components/tpl/tpl.js'),
     postlist = require('data/post_zh-cn.js');
 
 
+
+function dataMod (map, list) {
+    var i;
+    for (i = 0; i < map.length; i++) {
+        map[i][0] = {id: map[i][0], text: list.list[map[i][0]]};
+    }
+}
+
 function Cock() {
+
+    dataMod(postmap, postlist);
+
+    console.log( postmap );
+
+
 
 }
 
 
 module.exports = Cock;
+
+
+
+
+/*
+function sub (a, i) {
+    var num = 1;
+    while (i > 0) {
+        num = num * a;
+        i--;
+    }
+    return num;
+}
+
+function allnum (list) {
+    var i, len, num = 0;
+    for (i = 0, len = list.length; i < len; i++) {
+        num += list[i];
+    }
+    return num;
+}
+
+function fn (list, obj) {
+    var i;
+    if (!obj) {
+        obj = [];
+        for (i = 0; i < list.length; i++) {
+            console.log(i);
+            if (i) {
+
+            }
+
+        }
+    } else {
+
+    }
+
+}
+
+fn([1, 2, 3]);
+*/
