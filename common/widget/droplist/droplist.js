39a074b1a597dba5384bc1f55c387a020abade53
zsl99a <1033392732@qq.com>
2015-11-20 13:49:44 +0800

    代码更新

--- common/widget/droplist/droplist.js
+++ common/widget/droplist/droplist.js
@@ -23,7 +23,7 @@ var H = require('common:widget/helper/helper.js'),
 function drop($dom, data, style) {
     var v = $dom.data('v'),
         $i = $dom.children('i'),
-        callback = arguments[arguments.length-1];
+        callback = arguments[arguments.length - 1];
 
     $i.width($i.width()).addClass('ellipsis');
     if (!cache[v]) {
@@ -48,7 +48,6 @@ fn.create = function (data) {
     var $tpl = $('<div class="cw-droplist-i"><ul>'),
         $ul = $tpl.children('ul'),
         $tmp;
-
 
     for (var i = 0, len = data.length; i < len; i++) {
         $tmp = $('<li class="ellipsis">');
@@ -141,9 +140,7 @@ fn.move = function (type) {
                     top: top > 50 ? top : 50,
                     height: height
                 }, 200, function () {
-                    $tpl.css({
-                        overflow: ''
-                    });
+                    $tpl.css({overflow: ''});
                 });
             break;
         default :
@@ -170,8 +167,9 @@ $body.on('mouseenter', li, function () {
     var $self = $(this),
         v = $self.closest('.cw-droplist-i').attr('dropid'),
         $i = cache[v].$me.children('i');
-        
-    $i.text($self.attr('value') == 0 ? $i.attr('pla') : $self.text())
+
+    $self.addClass('active').siblings().removeClass('active');
+    $i.text(!$self.attr('value') ? $i.attr('pla') : $self.text())
         .siblings().val($self.attr('value'));
 });
 
